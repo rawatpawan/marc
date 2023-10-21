0d6eef54317f74a37830b5769e3d7befa0f9340d
Pawan Rawat <prawat816@gmail.com>
2023-10-21 19:51:28 +0530

    add nav form

--- js/script.js
+++ js/script.js
@@ -16,62 +16,56 @@
 
 // Mobile Navigation bar 
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//   // Get all dropdown toggle elements
-//   const dropdownToggleElements = document.querySelectorAll('.mobileMenu .nav-item.dropdown .nav-link.dropdown-toggle');
+  // Get all dropdown toggle elements
+  const dropdownToggleElements = document.querySelectorAll('.mobileMenu .nav-item.dropdown .nav-link.dropdown-toggle');
   
-//   // Add a click event listener to each dropdown toggle
-//   dropdownToggleElements.forEach((toggle) => {
-//     toggle.addEventListener('click', (e) => {
-//       // Prevent the default behavior (closing dropdown)
-//       e.preventDefault();
-//       e.stopPropagation();
+  // Add a click event listener to each dropdown toggle
+  dropdownToggleElements.forEach((toggle) => {
+    toggle.addEventListener('click', (e) => {
+      // Prevent the default behavior (closing dropdown)
+      e.preventDefault();
+      e.stopPropagation();
   
-//       // Toggle the 'show' class on the dropdown menu
-//       const dropdownMenu = toggle.nextElementSibling;
-//       if (dropdownMenu) {
-//         dropdownMenu.classList.toggle('show');
-//       }
-//     });
-//   });
+      // Toggle the 'show' class on the dropdown menu
+      const dropdownMenu = toggle.nextElementSibling;
+      if (dropdownMenu) {
+        dropdownMenu.classList.toggle('show');
+      }
+    });
+  });
   
-//   // Close dropdowns when clicking outside
-//   document.addEventListener('click', (e) => {
-//     dropdownToggleElements.forEach((toggle) => {
-//       // Close dropdowns that are not the target of the click
-//       if (toggle !== e.target) {
-//         const dropdownMenu = toggle.nextElementSibling;
-//         if (dropdownMenu && dropdownMenu.classList.contains('show')) {
-//           dropdownMenu.classList.remove('show');
-//         }
-//       }
-//     });
-//   });
+  // Close dropdowns when clicking outside
+  document.addEventListener('click', (e) => {
+    dropdownToggleElements.forEach((toggle) => {
+      // Close dropdowns that are not the target of the click
+      if (toggle !== e.target) {
+        const dropdownMenu = toggle.nextElementSibling;
+        if (dropdownMenu && dropdownMenu.classList.contains('show')) {
+          dropdownMenu.classList.remove('show');
+        }
+      }
+    });
+  });
   
-//   });
+  });
   
   
 
 
-
-
-
-
-
-
-  // $('[data-toggle="slide-collapse"]').on('click', function() {
-  //   $navMenuCont = $($(this).data('target'));
-  //   $navMenuCont.animate({
-  //     'width': 'toggle'
-  //   }, 350);
-  //   $(".menu-overlay").fadeIn(500);
+  $('[data-toggle="slide-collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.animate({
+      'width': 'toggle'
+    }, 350);
+    $(".menu-overlay").fadeIn(500);
   
-  // });
-  // $(".menu-overlay").click(function(event) {
-  //   $(".navbar-toggler").trigger("click");
-  //   $(".menu-overlay").fadeOut(500);
-  // });
+  });
+  $(".menu-overlay").click(function(event) {
+    $(".navbar-toggler").trigger("click");
+    $(".menu-overlay").fadeOut(500);
+  });
   
 
    $('.first').owlCarousel({
@@ -113,7 +107,7 @@ $('.third').owlCarousel({
           items:1
       },
       600:{
-          items:1
+          items:3
       },
       1000:{
           items:3
